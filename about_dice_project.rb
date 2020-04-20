ac3e091c6c17c988b75c8b95650b151c25325c73
Matt Kornfield <matt.kornfield@appian.com>
2020-04-19 21:10:05 -0400

    more done woot

--- about_dice_project.rb
+++ about_dice_project.rb
@@ -2,9 +2,16 @@ require File.expand_path(File.dirname(__FILE__) + '/neo')
 
 # Implement a DiceSet Class here:
 #
-# class DiceSet
-#   code ...
-# end
+class DiceSet
+  attr_accessor :values
+
+  def roll(num)
+    @values = []
+    for i in 1..num do
+      @values << rand(6) + 1
+    end
+  end
+end
 
 class AboutDiceProject < Neo::Koan
   def test_can_create_a_dice_set
@@ -40,8 +47,8 @@ class AboutDiceProject < Neo::Koan
     dice.roll(5)
     second_time = dice.values
 
-    assert_not_equal first_time, second_time,
-      "Two rolls should not be equal"
+    assert_not_equal first_time, second_time
+      # "Two rolls should not be equal"
 
     # THINK ABOUT IT:
     #
